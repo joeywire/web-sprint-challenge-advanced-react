5203eb40a3ab96e7279e92d6fa7003316f48b327
joeywire <61367542+joeywire@users.noreply.github.com>
2020-10-30 10:21:28 -0500

    Hooked up to API and properly grabbing data

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,7 +3,25 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor () {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
 
+  // fetchPlants = () => {
+
+  // }
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+      .then(res => {
+        this.setState({
+          plants: res.data.plantsData
+        })
+      })
+      .catch(err => console.log(err))
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
